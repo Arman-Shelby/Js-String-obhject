4cb1dcda12d7cf469aed2e02f008eaabc33a3558
Arman Mir <mirarman8583@gmail.com>
2025-08-01 08:31:41 +0600

    string

---
+++ string.js
@@ -0,0 +1,18 @@
+const country = 'Bangladesh';
+const division = "Noa khal e";
+const district = `B-Baria`;
+const thana = new String('Hatiya');
+
+// console.log(typeof thana);
+
+const numbers = [434, 3434, 553, 453, 5345 ,35 ];
+// console.log(numbers.length);
+// console.log(numbers[0]);
+
+const capital = 'Dh                   aka';
+// console.log(capital.length); //string length 
+// console.log(capital[23]); //Pick perticuler space in an arraay 
+console.log(capital);
+capital[0] = 'P';
+console.log(capital);
+//string is immutable means not changeable;;
