5563b517e9d18ee1d922f4cb3b15696db94f0038
Arman Mir <mirarman8583@gmail.com>
2025-08-03 08:59:16 +0600

    object

---
+++ object.js
@@ -0,0 +1,38 @@
+
+//Primitive 
+//Basic, PRimary Data Type 
+const age = 21;
+
+const school  = 'Rifle Square';
+const  isPassed  = true;
+let isDeveloper;
+const subjects = ['Bangla', 'English', 'Math', 'Physics'];
+const bottlek = ['White', 45, ];
+const bottleRong = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'Water';
+
+
+//Non-Primitive 
+//Object 
+const bottle = {
+    brand: 'Apple',
+    price: 45,
+    color: 'Water',
+    isClean: false
+
+}
+
+
+
+const subject = {
+    name: 'Biology',
+    teacher: 'Rasheda Maam',
+    examDate: '30 Feb',
+    chapter: ['First', 'Second', 'Third'],
+    exams:{
+        name: 'Final Exam',
+        marks: 100
+
+    }
+}
